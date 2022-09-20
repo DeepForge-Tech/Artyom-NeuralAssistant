23bdb0712dcef036c904e3ce26f3bb4419ed611c
blackflame576 <pws15548@gmail.com>
2022-09-20 14:30:14 +0300

    Deploy

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,19 +1,16 @@
-from itertools import tee
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
 import os 
 import json
-from sklearn.feature_extraction.text import CountVectorizer
-from tqdm import tqdm,trange
+from tqdm import trange
 from time import sleep
 
 np.random.seed(42)
 
 BATCH_SIZE = 50
 EPOCHS = 25000
-LOSS = 0
 
 CATEGORIES = ['communication','weather','youtube','webbrowser','music','news','todo','calendar','joikes']
 learning_rate = 0.0002
@@ -24,7 +21,7 @@ test_data = {}
 
 # Read data and setup maps for integer encoding and decoding.
 ProjectDir = os.getcwd()
-file = open('Datasets/MarcusDataset.json','r',encoding='utf-8')
+file = open('Datasets/ArtyomDataset.json','r',encoding='utf-8')
 DataFile = json.load(file)
 train_data = DataFile['train_dataset']
 test_data = DataFile['test_dataset']
@@ -37,17 +34,11 @@ print('%d unique words found' % vocab_size)
 word_to_idx = { w: i for i, w in enumerate(vocab) }
 idx_to_word = { i: w for i, w in enumerate(vocab) }
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def deriv_sigmoid(y):
-    return y * (1 - y)
 
 def tanh(x):
     return np.tanh(x)
 
 def softmax(xs):
-    # Applies the Softmax Function to the input array.
     return np.exp(xs) / sum(np.exp(xs))
 
 def deriv_tanh(y):
@@ -61,18 +52,21 @@ def MSE(PredictedValue,TargetValue):
     return Loss
 
 class NeuralNetwork():
-    def __init__(self,LENGHT_DATA:int,*args,**kwargs):
+    def __init__(self,LENGHT_DATA:int,OUTPUT_LAYERS,TargetArray,*args,**kwargs):
         self.Loss = 0
         self.LossArray = []
         self.Accuracy = 0
         self.AccuracyArray = []
         self.LocalLoss = 0.5
         self.LocalAccuracy = 1.0
+        self.Loss = 0
+        self.CorrectPredictions = 0
+        self.TargetArray = TargetArray
 
         self.LENGHT_DATA = LENGHT_DATA
         self.INPUT_LAYERS = self.LENGHT_DATA
         self.HIDDEN_LAYERS = 128
-        self.OUTPUT_LAYERS = 10
+        self.OUTPUT_LAYERS = OUTPUT_LAYERS
         # Weights
         self.whh = np.random.randn(self.HIDDEN_LAYERS, self.HIDDEN_LAYERS) / 1000
         self.wxh = np.random.randn(self.HIDDEN_LAYERS, self.INPUT_LAYERS) / 1000
@@ -169,53 +163,45 @@ class NeuralNetwork():
 
     def train(self,data:dict,BackPropagation):
         bar = trange(EPOCHS,leave=True)
-        iteration = 0
         for epoch in bar:
             items = list(data.items())
             random.shuffle(items)
 
-            loss = 0
-            num_correct = 0
-
             for x, y in items:
+                # Подготовка входных значений
                 Input = self.PreprocessingText(x)
                 Target = int(y)
-
-                # Forward
+                # Рассчёт ответа нейросети
                 Output, OutputHiddenLayer = self.FeedForward(Input)
                 PredictedValue = softmax(Output)
-
-                # Calculate loss / accuracy
-                # loss = MSE(Output,Target)
-                num_correct += int(np.argmax(PredictedValue) == Target)
+                self.Loss = MSE(Output,Target)
+                self.CorrectPredictions += int(np.argmax(PredictedValue) == Target)
+                # Обратное распространение ошибки
                 if BackPropagation:
                   # Build dL/dy
                   STG = PredictedValue
                   STG[Target] -= 1
                   # Backward Propagation
                   self.BackwardPropagation(STG)
-                train_loss, train_acc = loss / len(data), num_correct / len(data)
-                
+                # Расчёт количества ошибок и правильных ответов
+                train_loss, train_acc = self.Loss / len(data), self.CorrectPredictions / len(data)
+                # Сохранение лучших значений весов для последующего использования нейросети
                 if train_loss <= self.LocalLoss and train_acc == self.LocalAccuracy:
                     self.LocalLoss = train_loss
                     self.LocalAccuracy = train_acc
                     # print('Best model')
                     self.save()
                 bar.set_description(f'Epoch: {epoch}/{EPOCHS}; Loss: {train_loss}; Accuracy: {train_acc}')
-                # if iteration % 100 ==  0:
-                #     print(f'Epoch: {epoch}/{EPOCHS}; Loss: {train_loss}; Accuracy: {train_acc}')
-                # iteration += 1
                 LossArray.append(train_loss)
         plt.plot(LossArray)
         plt.show()
     def predict(self,data:str):
+        # Подготовка входных значений
         Input = self.PreprocessingText(data)
-        # Forward
+        # Рассчёт ответа нейросети
         Output, OutputHiddenLayer = self.FeedForward(Input)
-        PredictedValue = softmax(Output)
-        print(PredictedValue)
-        print('Argmax:' + str(np.argmax(Output)))
-        print(f'{CATEGORIES[np.argmax(Output)]}')
+        PredictedValue = np.argmax(Output)
+        print(f'{self.TargetArray[PredictedValue]}')
 
     def save(self):
         np.savez_compressed('Artyom_NeuralAssistant', self.whh,self.wxh,self.why,self.bh,self.by)
@@ -228,7 +214,7 @@ class NeuralNetwork():
         self.bh = ParametrsFile['arr_3']
         self.by = ParametrsFile['arr_4']
 
-network = NeuralNetwork(vocab_size)
+network = NeuralNetwork(vocab_size,len(CATEGORIES),CATEGORIES)
 network.train(train_data,True)
 network.train(train_data,False)
 
@@ -240,19 +226,19 @@ print('%d unique words found' % vocab_size)
 word_to_idx = { w: i for i, w in enumerate(vocab) }
 idx_to_word = { i: w for i, w in enumerate(vocab) }
 
-network = NeuralNetwork(vocab_size)
+network = NeuralNetwork(vocab_size,len(CATEGORIES),CATEGORIES)
 network.train(test_data,True)
 network.train(test_data,False)
 
 # Тест нейросети на скорость
-CorrectPredictions = 0
-items = list(train_data.items())
-random.shuffle(items)
-for x, y in items:
-    PredictedValue = network.predict(x)
-    if PredictedValue == int(y):
-        CorrectPredictions += 1
-print(CorrectPredictions)
+# CorrectPredictions = 0
+# items = list(train_data.items())
+# random.shuffle(items)
+# for x, y in items:
+#     PredictedValue = network.predict(x)
+#     if PredictedValue == int(y):
+#         CorrectPredictions += 1
+# print(CorrectPredictions)
 while True:
     InputData = input('Input data:')
     if InputData != 'stop':
@@ -263,7 +249,7 @@ while True:
         word_to_idx = { w: i for i, w in enumerate(vocab) }
         print(word_to_idx)
         idx_to_word = { i: w for i, w in enumerate(vocab) }
-        network = NeuralNetwork(vocab_size)
+        network = NeuralNetwork(vocab_size,len(CATEGORIES),CATEGORIES)
         PredictedValue = network.predict(InputData)
         print(PredictedValue)
     else:
